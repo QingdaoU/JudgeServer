031b0b617bc74fe4b0b09dd12d6ce8c7c15ae406
virusdefender <virusdefender@outlook.com>
2016-10-28 01:04:30 +0800

    cal cpu core number

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 chown compiler:compiler /spj
 echo 0 > /tmp/counter
-gunicorn --workers 4 --threads 4 --error-logfile /log/gunicorn.log --bind 0.0.0.0:8080 server:wsgiapp
+core=$(grep --count ^processor /proc/cpuinfo)
+n=$(($core*4))
+gunicorn --workers $n --threads $n --error-logfile /log/gunicorn.log --time 600 --bind 0.0.0.0:8080 server:wsgiapp
