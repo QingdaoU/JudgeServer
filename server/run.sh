69223da979d96edf395cedc303106d4bfffec3c1
virusdefender <virusdefender@outlook.com>
2017-03-18 02:02:37 +0800

    在 chmod 之前 chown，否则用户和用户组不是 root 的时候，可能不是预想的结果。

--- server/run.sh
+++ server/run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 chown compiler:compiler /spj
-echo 0 > /tmp/counter
 core=$(grep --count ^processor /proc/cpuinfo)
 n=$(($core*4))
-chmod 400 /token.txt /tmp/counter
-chmod -R 400 /test_case
+chown -R root:root /test_case /token.txt
+chmod -R 400 /test_case /token.txt
 gunicorn --workers $n --threads $n --error-logfile /log/gunicorn.log --time 600 --bind 0.0.0.0:8080 server:wsgiapp
