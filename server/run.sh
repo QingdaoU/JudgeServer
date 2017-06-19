9b139247fa1837483c63a95b8adf4542bdd3fca6
李扬 <virusdefender@users.noreply.github.com>
2017-06-19 20:06:18 +0800

    avoid too many workers

--- server/run.sh
+++ server/run.sh
@@ -2,7 +2,7 @@
 chown compiler:compiler /spj
 echo 0 > /tmp/counter
 core=$(grep --count ^processor /proc/cpuinfo)
-n=$(($core*4))
+n=$(($core*2))
 chmod 400 /token.txt /tmp/counter
 chmod -R 400 /test_case
 gunicorn --workers $n --threads $n --error-logfile /log/gunicorn.log --time 600 --bind 0.0.0.0:8080 server:wsgiapp
