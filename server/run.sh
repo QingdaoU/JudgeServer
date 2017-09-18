41ec65e15dcdd56f64f2595b4dc1667d21f5b63a
zema1 <rawidn@163.com>
2017-09-18 16:23:06 +0800

    remove chmod for token.txt in run.sh

--- server/run.sh
+++ server/run.sh
@@ -3,6 +3,6 @@ chown compiler:compiler /spj
 echo 0 > /tmp/counter
 core=$(grep --count ^processor /proc/cpuinfo)
 n=$(($core*2))
-chmod 400 /token.txt /tmp/counter
+chmod 400 /tmp/counter
 chmod -R 400 /test_case
 gunicorn --workers $n --threads $n --error-logfile /log/gunicorn.log --time 600 --bind 0.0.0.0:8080 server:wsgiapp
